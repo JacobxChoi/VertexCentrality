de06c6a5ff6db2b69595acf368fca6b6eef15529
choijacob1015 <choijacob1015@gmail.com>
2022-05-13 12:50:07 -0700

    VertexCentrality

---
+++ VertexCentrality.java
@@ -0,0 +1,512 @@
+/*
+THIS CODE IS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING
+A TUTOR OR CODE WRITTEN BY OTHER STUDENTS - Jacob Choi
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
+    
+class Bag<Item> implements Iterable<Item> {
+    private int N;               // number of elements in bag
+    private Node<Item> first;    // beginning of bag
+
+    // helper linked list class
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
+
+    /**
+     * Initializes an empty bag.
+     */
+    public Bag() {
+        first = null;
+        N = 0;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void add(Item item) {
+        Node<Item> oldfirst = first;
+        first = new Node<Item>();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+
+    /**
+     * Returns an iterator that iterates over the items in the bag in arbitrary order.
+     * @return an iterator that iterates over the items in the bag in arbitrary order
+     */
+    public Iterator<Item> iterator()  {
+        return new ListIterator<Item>(first);  
+    }
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class ListIterator<Item> implements Iterator<Item> {
+        private Node<Item> current;
+
+        public ListIterator(Node<Item> first) {
+            current = first;
+        }
+
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next; 
+            return item;
+        }
+    }
+}
+
+class EdgeWeightedDigraph {
+    private final int V;
+    private int E;
+    private Bag<DirectedEdge>[] adj;
+
+    public EdgeWeightedDigraph(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<DirectedEdge>[]) new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<DirectedEdge>();
+    }
+
+    public EdgeWeightedDigraph(int V, int E) {
+        this(V);
+        if (E < 0) throw new IllegalArgumentException("Number of edges in a Digraph must be nonnegative");
+        for (int i = 0; i < E; i++) {
+            int v = (int) (Math.random() * V);
+            int w = (int) (Math.random() * V);
+            double weight = Math.round(100 * Math.random()) / 100.0;
+            DirectedEdge e = new DirectedEdge(v, w, weight);
+            addEdge(e);
+        }
+    }
+
+    public EdgeWeightedDigraph(EdgeWeightedDigraph G) {
+        this(G.V());
+        this.E = G.E();
+        for (int v = 0; v < G.V(); v++) {
+            // reverse so that adjacency list is in same order as original
+            Stack<DirectedEdge> reverse = new Stack<DirectedEdge>();
+            for (DirectedEdge e : G.adj[v]) {
+                reverse.push(e);
+            }
+            for (DirectedEdge e : reverse) {
+                adj[v].add(e);
+            }
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    // throw an IndexOutOfBoundsException unless 0 <= v < V
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    public void addEdge(DirectedEdge e) {
+        int v = e.from();
+        int w = e.to();
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(e);
+        E++;
+    }
+
+    public Iterable<DirectedEdge> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    public int outdegree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
+
+    public Iterable<DirectedEdge> edges() {
+        Bag<DirectedEdge> list = new Bag<DirectedEdge>();
+        for (int v = 0; v < V; v++) {
+            for (DirectedEdge e : adj(v)) {
+                list.add(e);
+            }
+        }
+        return list;
+    } 
+
+    public String toString() {
+        String NEWLINE = System.getProperty("line.separator");
+        StringBuilder s = new StringBuilder();
+        s.append(V + " " + E + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (DirectedEdge e : adj[v]) {
+                s.append(e + "  ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+}
+    
+class DirectedEdge { 
+    private final int v;
+    private final int w;
+    private final double weight;
+
+    public DirectedEdge(int v, int w, double weight) {
+        if (v < 0) throw new IndexOutOfBoundsException("Vertex names must be nonnegative integers");
+        if (w < 0) throw new IndexOutOfBoundsException("Vertex names must be nonnegative integers");
+        if (Double.isNaN(weight)) throw new IllegalArgumentException("Weight is NaN");
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    public String toString() {
+        return v + "->" + w + " " + String.format("%5.2f", weight);
+    }
+}
+
+class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer> {
+    private int NMAX;        // maximum number of elements on PQ
+    private int N;           // number of elements on PQ
+    private int[] pq;        // binary heap using 1-based indexing
+    private int[] qp;        // inverse of pq - qp[pq[i]] = pq[qp[i]] = i
+    private Key[] keys;      // keys[i] = priority of i
+
+    public IndexMinPQ(int NMAX) {
+        if (NMAX < 0) throw new IllegalArgumentException();
+        this.NMAX = NMAX;
+        keys = (Key[]) new Comparable[NMAX + 1];    // make this of length NMAX??
+        pq   = new int[NMAX + 1];
+        qp   = new int[NMAX + 1];                   // make this of length NMAX??
+        for (int i = 0; i <= NMAX; i++) qp[i] = -1;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public boolean contains(int i) {
+        if (i < 0 || i >= NMAX) throw new IndexOutOfBoundsException();
+        return qp[i] != -1;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void insert(int i, Key key) {
+        if (i < 0 || i >= NMAX) throw new IndexOutOfBoundsException();
+        if (contains(i)) throw new IllegalArgumentException("index is already in the priority queue");
+        N++;
+        qp[i] = N;
+        pq[N] = i;
+        keys[i] = key;
+        swim(N);
+    }
+
+    public int minIndex() { 
+        if (N == 0) throw new NoSuchElementException("Priority queue underflow");
+        return pq[1];        
+    }
+
+    public Key minKey() { 
+        if (N == 0) throw new NoSuchElementException("Priority queue underflow");
+        return keys[pq[1]];        
+    }
+
+    /**
+     * Removes a minimum key and returns its associated index.
+     * @return an index associated with a minimum key
+     * @throws java.util.NoSuchElementException if priority queue is empty
+     */
+    public int delMin() { 
+        if (N == 0) throw new NoSuchElementException("Priority queue underflow");
+        int min = pq[1];        
+        exch(1, N--); 
+        sink(1);
+        qp[min] = -1;            // delete
+        keys[pq[N+1]] = null;    // to help with garbage collection
+        pq[N+1] = -1;            // not needed
+        return min; 
+    }
+
+    public Key keyOf(int i) {
+        if (i < 0 || i >= NMAX) throw new IndexOutOfBoundsException();
+        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
+        else return keys[i];
+    }
+
+    @Deprecated public void change(int i, Key key) {
+        changeKey(i, key);
+    }
+
+    public void changeKey(int i, Key key) {
+        if (i < 0 || i >= NMAX) throw new IndexOutOfBoundsException();
+        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
+        keys[i] = key;
+        swim(qp[i]);
+        sink(qp[i]);
+    }
+
+    public void decreaseKey(int i, Key key) {
+        if (i < 0 || i >= NMAX) throw new IndexOutOfBoundsException();
+        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
+        if (keys[i].compareTo(key) <= 0) throw new IllegalArgumentException("Calling decreaseKey() with given argument would not strictly decrease the key");
+        keys[i] = key;
+        swim(qp[i]);
+    }
+
+    public void increaseKey(int i, Key key) {
+        if (i < 0 || i >= NMAX) throw new IndexOutOfBoundsException();
+        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
+        if (keys[i].compareTo(key) >= 0) throw new IllegalArgumentException("Calling increaseKey() with given argument would not strictly increase the key");
+        keys[i] = key;
+        sink(qp[i]);
+    }
+
+    public void delete(int i) {
+        if (i < 0 || i >= NMAX) throw new IndexOutOfBoundsException();
+        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
+        int index = qp[i];
+        exch(index, N--);
+        swim(index);
+        sink(index);
+        keys[i] = null;
+        qp[i] = -1;
+    }
+
+    private boolean greater(int i, int j) {
+        return keys[pq[i]].compareTo(keys[pq[j]]) > 0;
+    }
+
+    private void exch(int i, int j) {
+        int swap = pq[i]; pq[i] = pq[j]; pq[j] = swap;
+        qp[pq[i]] = i; qp[pq[j]] = j;
+    }
+
+    private void swim(int k)  {
+        while (k > 1 && greater(k/2, k)) {
+            exch(k, k/2);
+            k = k/2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2*k <= N) {
+            int j = 2*k;
+            if (j < N && greater(j, j+1)) j++;
+            if (!greater(k, j)) break;
+            exch(k, j);
+            k = j;
+        }
+    }
+
+    public Iterator<Integer> iterator() { return new HeapIterator(); }
+
+    private class HeapIterator implements Iterator<Integer> {
+        // create a new pq
+        private IndexMinPQ<Key> copy;
+
+        // add all elements to copy of heap
+        // takes linear time since already in heap order so no keys move
+        public HeapIterator() {
+            copy = new IndexMinPQ<Key>(pq.length - 1);
+            for (int i = 1; i <= N; i++)
+                copy.insert(pq[i], keys[pq[i]]);
+        }
+
+        public boolean hasNext()  { return !copy.isEmpty();                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Integer next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return copy.delMin();
+        }
+    }
+}
+
+class DijkstraSP {
+    public double[] distTo;          // distTo[v] = distance  of shortest s->v path
+    private DirectedEdge[] edgeTo;    // edgeTo[v] = last edge on shortest s->v path
+    private IndexMinPQ<Double> pq;    // priority queue of vertices
+
+    public DijkstraSP(EdgeWeightedDigraph G, int s) {
+        for (DirectedEdge e : G.edges()) {
+            if (e.weight() < 0)
+                throw new IllegalArgumentException("edge " + e + " has negative weight");
+        }
+
+        distTo = new double[G.V()];
+        edgeTo = new DirectedEdge[G.V()];
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSITIVE_INFINITY;
+        distTo[s] = 0.0;
+
+        // relax vertices in order of distance from s
+        pq = new IndexMinPQ<Double>(G.V());
+        pq.insert(s, distTo[s]);
+        while (!pq.isEmpty()) {
+            int v = pq.delMin();
+            for (DirectedEdge e : G.adj(v))
+                relax(e);
+        }
+
+        // check optimality conditions
+       // assert check(G, s);
+    }
+
+    // relax edge e and update pq if changed
+    private void relax(DirectedEdge e) {
+        int v = e.from(), w = e.to();
+        if (distTo[w] > distTo[v] + e.weight()) {
+            distTo[w] = distTo[v] + e.weight();
+            edgeTo[w] = e;
+            if (pq.contains(w)) pq.decreaseKey(w, distTo[w]);
+            else                pq.insert(w, distTo[w]);
+        }
+    }
+
+    public double distTo(int v) {
+        return distTo[v];
+    }
+
+    public boolean hasPathTo(int v) {
+        return distTo[v] < Double.POSITIVE_INFINITY;
+    }
+
+    public Iterable<DirectedEdge> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+            path.push(e);
+        }
+        return path;
+    }
+
+}
+
+class Entry implements Comparable<Entry>{
+    private int key;
+    private double value;
+    
+    public Entry (int key, double value){
+        this.key = key;
+        this.value = value;
+    }
+    
+    public int getKey(){
+        return this.key;
+    }
+    public double getValue(){
+        return this.value;
+    }
+    
+    @Override
+    public int compareTo(Entry entry){
+        return Double.compare(entry.getValue(), this.getValue());
+    }
+}
+
+ class Result {
+
+    EdgeWeightedDigraph G;
+
+    public Result(int numVerticies){
+        G = new EdgeWeightedDigraph(numVerticies);
+    }
+    public void addWeightedEdge(int curOrigin, int curDestination, int curWeight) {
+        G.addEdge(new DirectedEdge(curOrigin, curDestination, curWeight));
+    }
+
+    public int mostInfluentialVertex() {
+
+        PriorityQueue<Entry> check3 = new PriorityQueue<>();
+
+        double sumDist = 0;
+        for(int i = 0; i < G.V(); i++){
+            DijkstraSP sp = new DijkstraSP(G,i);
+            for(int j = 0; j < G.V(); j++){
+                if(sp.hasPathTo(j) && sp.distTo(j) != Double.POSITIVE_INFINITY && sp.distTo(j) != 0){
+                    sumDist += 0.0 + 1/sp.distTo(j);
+                }
+            }
+            check3.add(new Entry(i, (G.V()-1)*sumDist));
+            sumDist = 0;
+        }
+        return check3.poll().getKey();
+    }
+}
+
+public class VertexCentrality {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int numVertices = Integer.parseInt(firstMultipleInput[0]);
+        int numEdges = Integer.parseInt(firstMultipleInput[1]);
+      
+        Result result = new Result(numVertices);
+
+        IntStream.range(0, numEdges).forEach(numEdgesItr -> {
+            try {
+                String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+                int curOrigin = Integer.parseInt(secondMultipleInput[0]);
+                int curDestination = Integer.parseInt(secondMultipleInput[1]);
+                int curWeight = Integer.parseInt(secondMultipleInput[2]);
+
+                result.addWeightedEdge(curDestination, curOrigin, curWeight);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        System.out.println(result.mostInfluentialVertex());
+        bufferedReader.close();
+    }
+}
